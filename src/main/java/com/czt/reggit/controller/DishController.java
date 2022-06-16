55d56bd11cb29718e67ee05f92250d79b265d141
Tao135 <1295072844@qq.com>
2022-06-16 21:23:49 +0800

    @Tao135 瑞吉外卖之菜品信息分页查询 注意事项： 1.因为展示出来的菜品包括菜品名称，所以不能简单的使用page<dish>，需要用page<dishDto>。 2.对象拷贝： BeanUtils.copyProperties(pageInfo,pageDto,"records");     //将pageInfo中除了records的属性拷贝到pageDto。 3.逻辑结构需要清晰。

--- src/main/java/com/czt/reggit/controller/DishController.java
+++ src/main/java/com/czt/reggit/controller/DishController.java
@@ -1,16 +1,21 @@
 package com.czt.reggit.controller;
 
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.czt.reggit.common.R;
 import com.czt.reggit.dto.DishDto;
+import com.czt.reggit.pojo.Category;
 import com.czt.reggit.pojo.Dish;
+import com.czt.reggit.service.CategoryService;
 import com.czt.reggit.service.DishFlavorService;
 import com.czt.reggit.service.DishService;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Slf4j
 @RestController
@@ -20,6 +25,8 @@ public class DishController {
     private DishFlavorService dishFlavorService;
     @Autowired
     private DishService dishService;
+    @Autowired
+    private CategoryService categoryService;
 
     /**
      * 新增菜品
@@ -32,5 +39,53 @@ public class DishController {
 
         dishService.saveWithFlavor(dishDto);
         return R.success("新增成功");
+    }
+
+    /**
+     * 菜品信息分页查询
+     * @param page
+     * @param pageSize
+     * @param name
+     * @return
+     */
+    @GetMapping("/page")
+    public R<Page> page(int page,int pageSize,String name){
+
+        //构造分页构造器对象
+        Page<Dish> pageInfo = new Page<>(page,pageSize);
+        Page<DishDto> pageDto = new Page<>();
+
+        //条件构造器
+        LambdaQueryWrapper<Dish> lqw = new LambdaQueryWrapper<>();
+        lqw.like(name != null,Dish::getName,name);
+        //添加排序条件
+        lqw.orderByDesc(Dish::getUpdateTime);
+
+        //执行分页查询
+        dishService.page(pageInfo,lqw);
+
+        //对象拷贝
+        BeanUtils.copyProperties(pageInfo,pageDto,"records");     //将pageInfo中除了records的属性拷贝到pageDto
+
+        List<Dish> records = pageInfo.getRecords();
+        List<DishDto> list = records.stream().map((item) -> {
+            DishDto dishDto = new DishDto();
+
+            BeanUtils.copyProperties(item,dishDto);     //将item的值拷贝到dishDto
+
+            Long categoryId = item.getCategoryId();//分类id
+            //根据id查询分类对象
+            Category category = categoryService.getById(categoryId);
+
+            if (category != null){
+                String categoryName = category.getName();
+                dishDto.setCategoryName(categoryName);
+            }
+            return dishDto;
+        }).collect(Collectors.toList());
+
+        pageDto.setRecords(list);
+
+        return R.success(pageDto);
     }
 }
